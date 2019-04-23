be7ba0f53e4b38cae61b923ac040fe176353d83c
robertmadril <robertmmadril@gmail.com>
2019-04-22 19:39:16 -0700

    add html require and server rout to survey html page

--- server.js
+++ server.js
@@ -10,3 +10,14 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Require routing files
+
+//require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// begins server
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
